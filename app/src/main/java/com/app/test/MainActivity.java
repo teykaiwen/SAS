030ee347a314fafb4c0f2a1bcbb1db85c7ed7a21
unknown <hamza.halabi@gmail.com>
2018-11-15 17:25:45 +0800

    add camera

--- app/src/main/java/com/app/test/MainActivity.java
+++ app/src/main/java/com/app/test/MainActivity.java
@@ -2,6 +2,7 @@ package com.app.test;
 
 import android.content.Intent;
 import android.os.Bundle;
+import android.provider.MediaStore;
 import android.support.design.widget.FloatingActionButton;
 import android.support.design.widget.Snackbar;
 import android.support.v7.app.AppCompatActivity;
@@ -42,6 +43,15 @@ public class MainActivity extends AppCompatActivity {
             public void onClick(View v) {
                 Intent intent = new Intent(MainActivity.this, HistoryActivity.class);
                 startActivity(intent);
+            }
+        });
+
+        Button camera_button = findViewById(R.id.camera_button);
+        camera_button.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent = new Intent(MediaStore.ACTION_IMAGE_CAPTURE);
+                startActivityForResult(intent, 0);
             }
         });
 
